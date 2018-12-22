d827174f31e0a2c09118f3627d9abf832139e2b1
Igor Kamyshev <ikamyshev@geeksforless.net>
2018-12-22 08:34:13 +0200

    Identity Server with hardcoded clients/users, protected API, ConsoleClient that uses Identity to access API

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,92 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var tokenResponse = await GetTokenUsingClientCredentialsAsync();
+
+            await CallAPIUsingTokenAsync(tokenResponse);
+
+            tokenResponse = await GetTokenUsingUserCredentialsAsync();
+
+            await CallAPIUsingTokenAsync(tokenResponse);
+        }
+
+        private static async Task<TokenResponse> GetTokenUsingClientCredentialsAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            return tokenResponse;
+        }
+
+        private static async Task<TokenResponse> GetTokenUsingUserCredentialsAsync()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            return tokenResponse;
+        }
+
+        private static async Task CallAPIUsingTokenAsync(TokenResponse tokenResponse)
+        {
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
